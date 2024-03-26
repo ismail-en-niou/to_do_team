8ac6905e51ead864343b9eb7e799915dc6cbe57e
Malikaelabderrahmani <malikarahmani67@gmail.com>
2024-03-26 15:54:28 +0000

    Update of login page

---
+++ login.js
@@ -0,0 +1,6 @@
+let username = document.getElementById("username");
+let password = document.getElementById("password");
+let submit = document.getElementById("submit");
+console.log(username);
+console.log(password);
+console.log(submit);
