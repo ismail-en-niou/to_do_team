332071ab99d3868a28714a3cc0a8d779aaaee0c6
Malikaelabderrahmani <145621908+Malikaelabderrahmani@users.noreply.github.com>
2024-03-26 23:31:12 +0000

    Update login.js

--- login.js
+++ login.js
@@ -1,6 +1,29 @@
-let username = document.getElementById("username");
-let password = document.getElementById("password");
+let username = document.getElementById("username").value;
+let password = document.getElementById("password").value;
 let submit = document.getElementById("submit");
-console.log(username);
-console.log(password);
-console.log(submit);
+let feedback = document.getElementById("feedback");
+
+submit.onclick = function(e) {
+    if (username.value.length < 1 || password.value.length < 1) {
+        feedback.textContent = "Veuillez remplir les champs demandés";
+        e.preventDefault();
+        feedback.style.display = "block";
+        //setTimeout(() => {
+        //feedback.style.display = "none";}, 3000);
+        return;   
+    }
+};
+
+const usersRef = firebase.firestore().collection('users');
+usersRef.where('username', '==', username).where('password', '==', password)
+.get()
+.then((querySnapshot) => { 
+    if (!querySnapshot.empty) {
+        feedback.textContent = "Welcome to TO_DO List";
+        feedback.style.color = "lightgreen";
+    }   
+    else {
+        feedback.textContent = "Username or Password incorrect !";
+    }
+}
+)
