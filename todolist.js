c1a7a90085901ff17d727efc43624f223cdbde95
Fatima EL ASRI <fatimaelassri041@gmail.com>
2024-03-30 02:42:13 +0000

    up addtask funct

--- todolist.js
+++ todolist.js
@@ -1,13 +1,13 @@
 function addTask() {
-    var taskInput = document.getElementById("newTask");
+    var newTask = document.getElementById("newTask");
     var taskList = document.getElementById("taskList");
     
     // add task
-    if (taskInput.value.trim() !== "") {
+    if (newTask.value.trim() !== "") {
       var li = document.createElement("li");
-      li.innerHTML = taskInput.value + '<input  type="submit" value="delete" class="delete" onclick="removeTask(this)">';
+      li.innerHTML = newTask.value + '<input  type="submit" value="delete" class="delete" onclick="removeTask(this)">';
       taskList.appendChild(li);
-      taskInput.value = "";
+      newTask.value = "";
     } else {
       alert("Please enter a task!");
 
